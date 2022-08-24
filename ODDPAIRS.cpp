367959e7bdb815b6db8f90d9e0a59f83d0aab954
Poulami Basu <98650837+Poulami40@users.noreply.github.com>
2022-08-24 22:47:49 +0530

    Create ODDPAIRS.cpp

---
+++ ODDPAIRS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    long long n;
+	    cin>>n;
+	    
+	    
+	    cout<<(n*n/2)<<endl;
+	}
+	return 0;
+}
