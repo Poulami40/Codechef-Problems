c69832e3d490771fef0cdcd9e61394f63f177da0
Poulami Basu <98650837+Poulami40@users.noreply.github.com>
2022-09-16 06:42:07 +0530

    Create CREDS.cpp

---
+++ CREDS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    cout<<(4*x+y*2)<<endl;
+	}
+	return 0;
+}
